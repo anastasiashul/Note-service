115959885f5bf90157e5c093c4b45b400703cf2f
anastasiashul <m2402570@edu.misis.ru>
2025-12-23 00:20:14 +0300

    Update test_1.py
    Теперь пути должны быть правильными, на всякий случай написала вывод для каждого файла

--- backend/tests/test_1.py
+++ backend/tests/test_1.py
@@ -2,13 +2,14 @@ def test_project_structure():
     import os
    
     required_files = [
-        '../app.py',
-        '../requirements.txt'
+        'app.py',
+        'requirements.txt'
         
     ]
     
     for file_path in required_files:
         assert os.path.exists(file_path), f"Файл {file_path} не найден"
+        print(f"{file_name} найден")
     
     print(" Структура проекта корректна")
 
